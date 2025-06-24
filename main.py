148c7f8831ab42fa731a852f57339d0598b6a44a
Muhammad-anzil-pm <anzilareef7@gmail.com>
2025-06-25 02:59:55 +0530

    started

---
+++ main.py
@@ -0,0 +1,37 @@
+import cv2
+import pandas as pd
+import yaml
+
+from ultralytics import YOLO
+
+model = YOLO('weights/best.pt')
+
+img_path = input("Enter the path of your image : ")
+img = cv2.imread(img_path)
+
+results = model.predict(img, conf = 0.6)
+
+result = results[0].boxes.cls
+
+#to list of tensors
+item = result.tolist()
+items = set()
+
+with open("data/data.yaml", 'r') as classes:
+    out = yaml.safe_load(classes)
+    for i in item:
+        i = int(i)
+        items.add(out['names'][i])
+
+df = pd.read_csv('data/nutrition.csv')
+df.set_index('name', inplace=True)
+
+try:
+    for i in items:
+        print(i)
+        print(df.loc[i])
+        print('\n')
+except:
+    print("food not found...")
+
+results[0].show()
